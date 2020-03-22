2b21cceee5d62cbb0fcbf4d578022c82ba8acd85
Khaled Adad <khaledadad@lewisu.edu>
2020-03-22 16:16:42 -0500

    Trying to get MVP

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 import PostsPage from "./components/PostsContainer/PostsPage";
 import SearchBar from "./components/SearchBar/SearchBarContainer";
 
 const App = () => {
-
-    return ( 
-      <div className = "App" > 
-        <nav>
-          <img src={"./assets/camera-logo.svg"} className="" alt="" />  
-          <SearchBar />
-        </nav>
-        <PostsPage />
-      </div>
-    );
+  return (
+    <div className="App">
+      <SearchBar />
+      <PostsPage />
+    </div>
+  );
 };
 
 export default App;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,24 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import data from "../dummy-data.js";
 
 const PostsPage = () => {
   // set up state for your data
+  const [state] = useState(dummyData);
+  console.log(state);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {" "}
+      {/* map through data here to return a Post and pass data as props to Post */}{" "}
+      {state.map(post => {
+        return <Post post={post} key={post.id} />;
+      })}{" "}
     </div>
   );
 };
 
 export default PostsPage;
-
